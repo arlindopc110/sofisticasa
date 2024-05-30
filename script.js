5d16a1f4b44d0679753c40cec39047207fd325b3
arlindopc110 <arlindopc110@gmail.com>
2024-05-30 15:39:53 -0300

    Adicionado click que fecha janela de menu_mobile

--- script.js
+++ script.js
@@ -12,4 +12,16 @@ function menuInvertido() {
 
 menuBurguer.addEventListener('click', mobile);
 menuInvertidoBotao.addEventListener('click', menuInvertido);
- 
+
+
+// Garante que o menu esteja oculto ao recarregar a página
+window.addEventListener('load', () => {
+    menuMobile.style.display = 'none';
+});
+
+// Fecha o menu ao clicar fora dele
+document.addEventListener('click', function (event) {
+    if (menuMobile.style.display === 'block' && !menuMobile.contains(event.target) && !menuBurguer.contains(event.target)) {
+        menuMobile.style.display = 'none';
+    }
+});
